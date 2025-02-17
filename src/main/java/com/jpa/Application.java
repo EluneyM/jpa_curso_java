864b3f3923ae5abeb4abaf6dec66607cb03a1f6c
Matias <eluneymerillan@gmail.com>
2025-02-17 20:13:44 -0300

    Probar metodos

--- src/main/java/com/jpa/Application.java
+++ src/main/java/com/jpa/Application.java
@@ -1,6 +1,7 @@
 package com.jpa;
 
 import com.jpa.servicios.ClienteServicio;
+import com.jpa.servicios.EmpleadoServicio;
 import com.jpa.servicios.OficinaServicio;
 import com.jpa.servicios.ProductoServicio;
 
@@ -10,6 +11,7 @@ public class Application {
         OficinaServicio oficinaServicio = new OficinaServicio();
         ClienteServicio clienteServicio = new ClienteServicio();
         ProductoServicio productoServicio = new ProductoServicio();
+        EmpleadoServicio empleadoServicio = new EmpleadoServicio();
 
         try {
 
@@ -27,6 +29,21 @@ public class Application {
             System.out.println("*****************PRODUCTOS DISPONIBLES******************");
             System.out.println("ID PRODUCTO // NOMBRE PRODUCTO // DESCRIPCION PRODUCTO");
             productoServicio.listarProductos();
+
+            // IMPRIMIR TODOS LOS CLIENTE
+            System.out.println("****************CLIENTES POR NOMBRE*****************");
+            System.out.println("ID CLIENTE // APELLIDO CLIENTE // NOMBRE CLIENTE");
+            clienteServicio.listarClientes("Antonio");
+
+            // IMPRIMIR TODOS LOS CLIENTE
+            System.out.println("****************EMPLEADO POR OFICINA*****************");
+            System.out.println("ID EMPLEADO // APELLIDO EMPLEADO // NOMBRE EMPLEADO");
+            empleadoServicio.listarEmpleadosPorOficina(6);
+
+            // IMPRIMIR TODOS LOS CLIENTE
+            System.out.println("****************CLIENTES POR NOMBRE*****************");
+            System.out.println("ID CLIENTE // APELLIDO CLIENTE // NOMBRE CLIENTE");
+            clienteServicio.listarClientesPorCiudad("Madrid");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
